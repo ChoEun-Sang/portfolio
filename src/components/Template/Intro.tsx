1ea7d15b233447a26d3bfe1d903f193617f5799d
ChoEun-Sang <wh94es@gmail.com>
2024-05-14 19:42:10 +0900

    refactor: globals.css와 Intro 페이지 스타일 변경

--- src/components/Template/Intro.tsx
+++ src/components/Template/Intro.tsx
@@ -6,23 +6,22 @@ interface IntroProps {
 }
 function Intro({ scrollTo }: IntroProps) {
   return (
-    <section className="w-full h-screen">
+    <section className="w-screen h-screen flex flex-col items-center">
       <div
-        className="z-[-1] absolute inset-0 bg-cover bg-center "
+        className="w-full h-full bg-cover bg-center "
         style={{ backgroundImage: 'url("/mypic.png")' }}
       >
-        <div className="absolute inset-0 bg-black opacity-50" />
-        <div className="animate-pulse flex flex-col items-center gap-4 absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-xl md:text-3xl font-bold">
-          <span className="">안녕하세요!</span>
+        <div className="w-full h-full bg-black opacity-50" />
+        <div className="animate-pulse flex flex-col items-center gap-4 absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-sm md:text-3xl font-bold">
+          <span>안녕하세요!</span>
           <span>사용자를 먼저 생각하는 개발자,</span>
           <span>조은상입니다.</span>
         </div>
         <button
           type="button"
           onClick={scrollTo}
-          className="absolute bottom-10 left-1/2 transform -translate-x-1/2 translate-y-1/2 w-[60px] y-[60px]"
+          className=" absolute bottom-10 left-1/2 transform -translate-x-1/2 translate-y-1/2 w-[60px] y-[60px]"
         >
-          {" "}
           <Image
             src={arrowDown}
             width={60}
