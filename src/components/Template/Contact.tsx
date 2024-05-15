1abb3d8f189cd56adf6d3b8a0c5ef6c23b849054
ChoEun-Sang <wh94es@gmail.com>
2024-05-15 12:23:56 +0900

    refactor: 미입력 api 호출 시 alert 설정 및 연락처 컴포넌트 스타일 수정

--- src/components/Template/Contact.tsx
+++ src/components/Template/Contact.tsx
@@ -23,6 +23,9 @@ function Contact() {
   };
 
   const insertContact = async () => {
+    if (!contactData.name || !contactData.contact)
+      return alert("내용을 입력해주세요.");
+
     const { error } = await client.from("contact").insert({
       name: contactData.name,
       contact: contactData.contact,
@@ -50,14 +53,14 @@ function Contact() {
   };
 
   return (
-    <div>
-      <main className="mt-20 w-screen h-full flex flex-col justify-center items-center">
+    <main className="py-10 flex justify-center items-center">
+      <section className="p-10 md:w-1/2 max-md:w-3/4 flex flex-col justify-center items-center bg-gray-700 rounded-lg shadow-xl">
         <h1>연락처 등록</h1>
         <form className="flex flex-col gap-5 mt-6">
           <div>
             <label
               htmlFor="이름"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              className="block mb-2 text-sm font-medium text-white"
             >
               이름
             </label>
@@ -67,7 +70,7 @@ function Contact() {
               name="name"
               onChange={onChange}
               value={contactData.name}
-              className="bg-gray-500 border border-gray-300 text-gray-600 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className=" border text-sm rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white "
               placeholder="이름"
               required
             />
@@ -75,7 +78,7 @@ function Contact() {
           <div>
             <label
               htmlFor="message"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              className="block mb-2 text-sm font-medium text-white"
             >
               연락처
             </label>
@@ -84,31 +87,31 @@ function Contact() {
               name="contact"
               onChange={onChange}
               value={contactData.contact}
-              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="block p-2.5 w-full text-sm  rounded-lg border bg-gray-700 border-gray-600 placeholder-gray-400 text-white "
               placeholder="연락처를 남겨주세요."
             ></textarea>
           </div>
           <Button onClick={insertContact} className="shrink-0">
             <span>등록</span>
           </Button>
-        </form>
 
-        {contactList.length > 0 && (
-          <ul className="mt-5 flex flex-col gap-3">
-            {contactList.map((contact, idx) => (
-              <li key={contact.id}>
-                <div>
-                  <strong>
-                    {idx + 1}. {maskName(contact.name)}
-                  </strong>
-                  <p> {truncateContact(contact.contact, 5)}</p>
-                </div>
-              </li>
-            ))}
-          </ul>
-        )}
-      </main>
-    </div>
+          {contactList.length > 0 && (
+            <ul className="mt-5 flex flex-col gap-3">
+              {contactList.map((contact, idx) => (
+                <li key={contact.id}>
+                  <div>
+                    <strong>
+                      {idx + 1}. {maskName(contact.name)}
+                    </strong>
+                    <p> {truncateContact(contact.contact, 5)}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+        </form>
+      </section>
+    </main>
   );
 }
 
